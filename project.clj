f18ba5af81b6abaabdc866dd423a55ea4f777700
Matthew Schulkind <mschulkind@gmail.com>
2015-04-07 16:25:00 -0400

    Adding some project metadata.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-auto-install "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A leiningen plugin to automatically run install for checkouts."
+  :url "https://github.com/hyperscience/lein-auto-install"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[digest "1.4.4"]
