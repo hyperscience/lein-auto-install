e9178681b26eda654d87e51bb814df2b446193ae
Matthew Schulkind <mschulkind@gmail.com>
2015-04-07 15:29:55 -0400

    Support nested checkouts.

--- src/lein_auto_install/plugin.clj
+++ src/lein_auto_install/plugin.clj
@@ -10,23 +10,49 @@
 (defn- slurp-if-exists [filename]
   (let [f (io/file filename)] (when (.exists f) (slurp f))))
 
+(defn- project-hash [project-root-dir]
+  "Returns the hash of the projects.clj in project-root-dir."
+  (let [f (io/file project-root-dir "project.clj")]
+    (if-let [c (slurp-if-exists f)]
+      (digest/md5 c)
+      (throw (RuntimeException. (str (.getPath f) " not found."))))))
+
+(defn- checkouts-tree
+  "Returns a tree of checkout paths of all checkouts from the project in
+  project-root-dir. The tree format is:
+  ((checkout1-path (checkouts-tree checkout1-path))
+   (checkout2-path ...)
+   ...)"
+  ([project-root-dir] (checkouts-tree project-root-dir #{}))
+  ([project-root-dir already-seen]
+   (map #(let [path (.getPath %)
+               h (project-hash %)]
+           (assert (not (already-seen h)) "Cycle found in dependencies.")
+           (cons path (checkouts-tree % (conj already-seen h))))
+        (.listFiles (io/file project-root-dir "checkouts")))))
+
+(defn- traverse-seq
+  "Takes a tree of checkout paths as returned by checkouts-tree and returns a
+  seq of checkout paths representing a post order traversal."
+  [trees]
+  (mapcat #(concat (traverse-seq (rest %)) [(first %)]) trees))
+
 (defn- install-checkouts [f & args]
   (let [project (first args)
         state-file (io/file (:root project) ".lein-auto-install")
-        last-state (when-let [c (slurp-if-exists state-file)] (read-string c))
-        current-state
-        (for-map [checkout (.listFiles (io/file (:root project) "checkouts"))]
-          (.getName checkout)
-          (digest/md5 (slurp (io/file checkout "project.clj"))) )]
+        last-state (set
+                    (when-let [c (slurp-if-exists state-file)] (read-string c)))
+        checkouts-seq (traverse-seq (checkouts-tree (:root project)))
+        ; maps project path -> project.clj hash
+        project-hash-map (for-map [c checkouts-seq] c (project-hash c))]
     (with-programs [lein]
-      (doseq [checkout (keys current-state)]
-        (when (not= (current-state checkout) (get last-state checkout))
-          (lein "install"
-                {:out *out*, :err *err*
-                 :dir (io/file (:root project) "checkouts" checkout)})
+      (doseq [c checkouts-seq]
+        (when-not (last-state (project-hash-map c))
+          (lein "install" {:out *out*, :err *err* :dir c})
           (flush))))
-    (when (not= last-state current-state)
-      (spit state-file (pr-str current-state))))
+    (let [current-state (set (vals project-hash-map))]
+      (when (not= last-state current-state)
+        (spit state-file (pr-str current-state)))))
   (apply f args))
 
 (defn hooks []
