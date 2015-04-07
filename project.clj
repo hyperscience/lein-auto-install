ea25a515d604ed405cd878b61a2847d6e2466836
Matthew Schulkind <mschulkind@gmail.com>
2015-04-07 16:25:17 -0400

    Preparing for 0.1.0 release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-auto-install "0.1.0-SNAPSHOT"
+(defproject lein-auto-install "0.1.0"
   :description "A leiningen plugin to automatically run install for checkouts."
   :url "https://github.com/hyperscience/lein-auto-install"
   :license {:name "Eclipse Public License"
